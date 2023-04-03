bee32ef614cb6eac60626aad75a933c206393df5
zarei.m <zarei.m@fadaktrains.com>
2023-04-03 16:28:51 +0430

    implement state managment redix with saga middleware

--- config-overrides.js
+++ config-overrides.js
@@ -3,9 +3,12 @@ const path = require('path');
 module.exports = function override(config) {
 	config.resolve = {
 		...config.resolve,
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
 		alias: {
 			...config.alias,
-			'@src': path.resolve(__dirname, './src'),
+			src: path.resolve(__dirname, './src/'),
+			store: path.resolve(__dirname, './src/store/'),
+			service: path.resolve(__dirname, './src/store/'),
 		},
 	};
 
